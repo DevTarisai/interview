c027842a15160adc5a839e010bf8592df94126a8
Tarisai Mangoma <121206140+DevTarisai@users.noreply.github.com>
2023-07-14 16:38:06 +0200

    foot

--- include/footer.php
+++ include/footer.php
@@ -37,7 +37,3 @@ E-mail: <font color="#dddddd">mail@zimttech.org</font></p>
         </div>
 </div>
 
-
-<div class="mapbg">
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d24781.309643812125!2d88.43504219827918!3d22.587938892232376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1456488315433" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-</div>
